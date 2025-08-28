6b5a7e5a21ca763cfd36cbf680055b40444c6122
vvania-ssantos <supervaniaa@gmail.com>
2025-08-28 17:07:26 -0300

    Adicionando limpeza de dados e exploracao inicial

--- src/projeto_manutencao_preditiva.py
+++ src/projeto_manutencao_preditiva.py
@@ -12,3 +12,42 @@ df = pd.read_csv(caminho_arquivo)
 
 # Imprimindo as primeiras 5 linhas do DataFrame
 print(df.head())
+
+print(df.info())
+
+print(df['falha_maquina'].value_counts())
+
+# Convertendo todos os valores da coluna 'falha_maquina' para minúsculas
+df['falha_maquina'] = df['falha_maquina'].str.lower()
+
+# Unificando os valores que indicam 'sem_falha'
+df['falha_maquina'] = df['falha_maquina'].replace(['não', 'n', '0'], 'sem_falha')
+
+# Unificando os valores que indicam 'com_falha' para um melhor entendimento
+df['falha_maquina'] = df['falha_maquina'].replace(['sim', 'y', '1'], 'com_falha')
+
+# Imprimindo a contagem de valores para verificar a mudança visual
+print(df['falha_maquina'].value_counts())
+
+# Imprime a contagem de valores para a coluna de 'Falha Desgaste Ferramenta'
+print(df['FDF (Falha Desgaste Ferramenta)'].value_counts())
+
+# Unificação dos valores que indicam 'sem falha' para 0
+df['FDF (Falha Desgaste Ferramenta)'] = df['FDF (Falha Desgaste Ferramenta)'].replace(['False', 'N', '0', '-'], 0)
+
+# Unificação dos valores que indicam 'com falha' para 1
+df['FDF (Falha Desgaste Ferramenta)'] = df['FDF (Falha Desgaste Ferramenta)'].replace(['True', '1'], 1)
+
+# Imprimindo a contagem de valores para verificar a mudança visual
+print(df['FDF (Falha Desgaste Ferramenta)'].value_counts())
+
+print(df['FDC (Falha Dissipacao Calor)'].value_counts())
+
+# Unificando os valores que indicam 'sem falha' para 0
+df['FDC (Falha Dissipacao Calor)'] = df['FDC (Falha Dissipacao Calor)'].replace(['False', 'nao', '0'], 0)
+
+# Unificando os valores que indicam 'com falha' para 1
+df['FDC (Falha Dissipacao Calor)'] = df['FDC (Falha Dissipacao Calor)'].replace(['True', 'y', '1'], 1)
+
+# Imprimindo a contagem de valores para verificar a mudança
+print(df['FDC (Falha Dissipacao Calor)'].value_counts())
